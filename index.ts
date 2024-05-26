757cce5b66b80640a3b4e2c9e11002908cebe4a1
Howard Sun <komugi@gmail.com>
2024-05-26 16:00:55 -0400

    Added: - Finished cheerio webscraper
    Removed:
    - Jest testing
    
    Changes to be committed:
    	modified:   index.ts
    	modified:   package-lock.json
    	modified:   package.json

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express from 'express';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import dotenv from 'dotenv';
@@ -7,6 +7,29 @@ dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+export interface Article {
+  title: string;
+  url: string;
+}
+
+const URL = 'https://www.theguardian.com/us';
+
+axios(URL)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html); // This loads the HTML and returns a Cheerio object
+    const articles: Article[] = [];
+
+    $('[class^="dcr-"]').each(function () {
+      const title = $(this).text();
+      const url = $(this).find('a').attr('href');
+      articles.push({ title, url: url || '' });
+    });
+
+    console.log(articles);
+  })
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`[server] Server listening at http://localhost:${PORT}`);

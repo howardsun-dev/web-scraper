ee594077641da77c99ac3d463980551a3a010367
Howard Sun <komugi@gmail.com>
2024-05-27 17:57:18 -0400

    Added: Puppeteer Changes to be committed: 	modified:   index.ts 	modified:   package-lock.json 	modified:   package.json

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import path from 'path';
 
 dotenv.config(); // Load environment variables
 
@@ -11,6 +13,32 @@ const PORT = process.env.PORT || 3000;
 
 const URL = 'https://www.theguardian.com/us';
 
+async function startPuppeteer() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    // Add a delay to ensure the main frame is ready
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+
+    await page.goto(URL, { waitUntil: 'networkidle2' }); // Wait until the network is idle
+
+    // Ensure the directory exists
+    const dir = './scrapedData';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    // Take a screenshot of the path to the specified path
+    const screenshotPath = path.resolve(dir, 'screenshot.png');
+    await page.screenshot({ path: screenshotPath, fullPage: true });
+    console.log('Took a screenshot of the webpage');
+  } catch (error) {
+    console.error('Error during Puppeteer operation:', error);
+  } finally {
+    await browser.close();
+  }
+}
 async function scrapeAndSaveArticles() {
   try {
     const html = await axios.get(URL);
@@ -23,17 +51,25 @@ async function scrapeAndSaveArticles() {
       articles.push({ title, url: url || '' });
     });
 
+    // Ensure the directory exists
+    const dir = './scrapedData';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
     // Save articles to a file
-    fs.writeFile('articles.json', JSON.stringify(articles), err => {
-      if (err) throw err;
-      console.log('Articles saved to articles.json');
-    });
+    fs.writeFile(
+      path.resolve(dir, 'articles.json'),
+      JSON.stringify(articles),
+      err => {
+        if (err) throw err;
+        console.log('Articles saved to articles.json');
+      },
+    );
   } catch (err) {
     console.error('Error during scraping:', err);
   }
 }
-
-scrapeAndSaveArticles();
 
 // Axios chain
 // axios(URL)
@@ -55,6 +91,12 @@ scrapeAndSaveArticles();
 //     });
 //   })
 //   .catch(err => console.log(err));
+async function main() {
+  // await scrapeAndSaveArticles();
+  await startPuppeteer();
+}
+
+main();
 
 app.listen(PORT, () => {
   console.log(`[server] Server listening at http://localhost:${PORT}`);
